24a9445b340c71492cdfac04845b894a2540e7e4
Tristan Daniel <116753684+Tristand1220@users.noreply.github.com>
2026-03-14 00:25:09 -0400

    Annotations kept at fix sizes; moves centroid of shape
    Test pitch, yaw, and both across 3 triangles with improved frame rate sine simulations

--- tft_color.ino
+++ tft_color.ino
@@ -37,19 +37,11 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 //Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
 // Will be collected from IMU
-float headPitch = 0.0;
-float headRoll = 0.0;
-float headYaw = 0.0;
-
-// Coordinate points within the world
-struct WorldPoint{
-  float x, y;
-};
-
-// Example of "object"  in real world
-WorldPoint s1 = {0.0, 5.0};
-WorldPoint s2 = {-5.0, -5.0};
-WorldPoint s3 = {5.0, -5.0};
+float headPitch = 0.0;  // Up and down;  Purposes for isolated testing of "yaw" (won't chnage in loop for triangle 1)
+float headPitch2 = 0.0; 
+float headRoll = 0.0;   // Head rotation
+float headYaw = 0.0;    // Left and right
+float headYaw2 = 0.0;   // Purposes for isolated testing of "pitch" (won't chnage in loop for triangle 3)
 
 /*
 ---------------------------------
@@ -64,29 +56,25 @@ SCREEN_W/2.0 = Scales to pixel rnage
 
 int worldToScreenX(float worldAngle, float headYaw){
   float delta = worldAngle - headYaw;
-  return (int)(delta / (FOV_H / 2.0) + 1.0) * (SCREEN_W/2.0);
+  //Serial.println((int)(delta / (FOV_H / 2.0) + 1.0) * (SCREEN_W/2.0));
+  return (int)((delta / (FOV_H / 2.0) + 1.0) * (SCREEN_W/2.0));
 
 }
 
 int worldToScreenY(float worldAngle, float headPitch){
   float delta = worldAngle - headPitch;
-  return (int)(delta / (FOV_V / 2.0) + 1.0) * (SCREEN_H/2.0);
+  return (int)((delta / (FOV_V / 2.0) + 1.0) * (SCREEN_H/2.0));
 
 }
 
-//Starting off by annotating triangles
-void drawTriangle(WorldPoint a, WorldPoint b, WorldPoint c, uint16_t color) {
-    int ax = worldToScreenX(a.x, headYaw),   ay = worldToScreenY(a.y, headPitch);
-    int bx = worldToScreenX(b.x, headYaw),   by = worldToScreenY(b.y, headPitch);
-    int cx = worldToScreenX(c.x, headYaw),   cy = worldToScreenY(c.y, headPitch);
-
-    tft.drawLine(ax, ay, bx, by, color);
-    tft.drawLine(bx, by, cx, cy, color);
-    tft.drawLine(cx, cy, ax, ay, color);
+// Draws triangle at fixed size, centerX and centerY are anchor points that will be updated as head motion occurs
+void drawTriangleCenter(int centerX, int centerY, uint16_t color) {
+    tft.drawTriangle(centerX, centerY-15, centerX-15, centerY+15, centerX+15, centerY+15, color);
 }
 
 void setup(void) {
   Serial.begin(9600);
+  
 
   // use this initializer (uncomment) if using a 0.96" 160x80 TFT:
   tft.initR(INITR_MINI160x80);  // Init ST7735S mini display
@@ -105,11 +93,47 @@ void setup(void) {
 void loop() {
   tft.invertDisplay(true);
 
+  // Frame timer
+  static unsigned long lastFrame = 0;
+  unsigned  long now = millis();
 
-  // Starting with simulating head motion with sine wave (for smoothness), essentially 30 degree "head swings" within 6 seconds
-  headYaw = 30.0 * sin(millis() / 1000.0);
+  // Skip redraw if frame hasn't reached 33 fps limit
+  if (now - lastFrame < 33) return;
+  lastFrame = now;
+
+
+  // Starting with simulating head motion with sine wave (for smoothness), essentially 30 degree "head swings"
+  headYaw = 30.0 * sin((float)now / 2000.0f);
+  headPitch2 = 30.0 * sin((float)now / 2000.0f);
+
+  //Initilaize anchor of the real world object
+  float anchorWorldX = 0.0;
+  float anchorWorldY = 0.0;
+
+  // Real object 2 - starting from different starting point in real world
+  float anchorWorldX2 = 1.0;
+  float anchorWorldY2 = 1.0;
+
+  // Real object 3 - starting from different starting point in real world
+  float anchorWorldX3 = 5.0;
+  float anchorWorldY3 = 5.0;
+
+  //Triangle 1 - focused on yaw changes (horizontal motion)
+  int cx = worldToScreenX(anchorWorldX, headYaw);
+  int cy = worldToScreenY(anchorWorldY, headPitch);
+
+  //Triangle 2 - focused on pitch + yaw changes (diagonal motion)
+  int cx2 = worldToScreenX(anchorWorldX2, headYaw);
+  int cy2 = worldToScreenY(anchorWorldY2, headPitch2);
+
+  //Triangle 3 - focused on pitch (vertcial motion)
+  int cx3 = worldToScreenX(anchorWorldX3, headYaw2);
+  int cy3 = worldToScreenY(anchorWorldY3, headPitch2);
 
   tft.fillScreen(ST77XX_BLACK);
-  drawTriangle(s1, s2, s3, ST77XX_CYAN);
-  delay(33); //33 fps
+
+  // Redrawing triangles based on changing anchored center points (cx.cy)
+  drawTriangleCenter(cx, cy, ST77XX_CYAN);
+  drawTriangleCenter(cx2, cy2, ST77XX_RED);
+  drawTriangleCenter(cx3, cy3, ST77XX_WHITE);
 }
